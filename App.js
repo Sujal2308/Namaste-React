bdcfc92ce9fbdce258d71f5b17093c547c9ff48c
Sujal <sujalbhugul08@gmail.com>
2023-12-07 19:45:50 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,5 @@
+let heading = React.createElement("h1" , {} , "Ram Ram Bhai Sariyane");
+let root = ReactDOM.createRoot(document.getElementById("root"));
+/* Note :- root and heading are react elements which are objects at the end of the 
+        and not a so called html elements */
+root.render(heading);
