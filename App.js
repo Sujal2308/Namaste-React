4d9a1a3855d0d6e9eb459bbc904801e54b1bd806
Sujal <sujalbhugul08@gmail.com>
2023-12-07 22:23:05 +0530

    Adding listItems

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
-let heading = React.createElement("h1" , {} , "Ram Ram Bhai Sariyane");
 let root = ReactDOM.createRoot(document.getElementById("root"));
-/* Note :- root and heading are react elements which are objects at the end of the 
-        and not a so called html elements */
+
+let heading = React.createElement("div" , {id : "parent"} , 
+[React.createElement("h1" , {} , "Heading 1")],
+[React.createElement("h3" , {} , "Heading 2")]);
+
+/* Note :- root and heading are react elements which are objects at the end of the day*/
 root.render(heading);
+
+
+let rootList = ReactDOM.createRoot(document.getElementById("rootList"));
+let list = React.createElement("ul", {id : "my_list"} , 
+[React.createElement("li" , {} , "Home")],
+[React.createElement("li" , {} , "Courses")],
+[React.createElement("li" , {} , "Catalogue")],
+[React.createElement("li" , {} , "Products")]
+)
+
+
+rootList.render(list);
+// rootList.render(list)
