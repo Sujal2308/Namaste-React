e7aa5bd568ceeba0f672ac67364b0f8cc775fbab
Sujal <sujalbhugul08@gmail.com>
2023-12-08 22:43:33 +0530

    set up react using npm

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-let root = ReactDOM.createRoot(document.getElementById("root"));
-
-let heading = React.createElement("div" , {id : "parent"} , 
-[React.createElement("h1" , {} , "Heading 1")],
-[React.createElement("h3" , {} , "Heading 2")]);
-
-/* Note :- root and heading are react elements which are objects at the end of the day*/
-root.render(heading);
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 let rootList = ReactDOM.createRoot(document.getElementById("rootList"));
 let list = React.createElement("ul", {id : "my_list"} , 
 [React.createElement("li" , {} , "Home")],
 [React.createElement("li" , {} , "Courses")],
 [React.createElement("li" , {} , "Catalogue")],
-[React.createElement("li" , {} , "Products")]
-)
-
+[React.createElement("li" , {} , "Products")])
 
 rootList.render(list);
-// rootList.render(list)
